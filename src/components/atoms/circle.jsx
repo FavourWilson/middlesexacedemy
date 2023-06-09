05b20bd538e7d6cd65921610a7c82d273215bc81
Favour Ebrusike <ebrusikefavour@gmail.com>
2023-06-09 13:19:52 +0200

    line code

--- src/components/atoms/circle.jsx
+++ src/components/atoms/circle.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Circle = ({className,value}) => {
+export const BigCircle = ({className,value}) => {
   return (
     <div className={`flex items-center gap-5 ${className}`}>
       <div className={`w-[3rem] h-[3rem] rounded-full bg-[#EB6707] `}></div>
@@ -9,4 +9,11 @@ const Circle = ({className,value}) => {
   )
 }
 
-export default Circle
+export const SmallCircle = ({className,value}) => {
+  return (
+    <div className={`flex items-center gap-5 ${className}`}>
+      <div className={`w-[1rem] h-[1rem] rounded-full bg-[#EB6707] `}></div>
+      <label className={ `text-[#EB6707] font-bold`}>{value}</label>
+    </div>
+  )
+}
---
+++ src/components/atoms/line.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Line = () => {
+  return (
+    <div className="p-5 h-[120rem] bg-slate-800 rounded-xl"></div>
+  )
+}
+
+export default Line
